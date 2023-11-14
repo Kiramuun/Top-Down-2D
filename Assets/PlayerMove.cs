1616c23b6f6713ea0059bf743e0d099d6ff5d334
Kiramuun <deimonlight@live.fr>
2023-11-14 21:18:00 +0100

    Update

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    Vector2 _movementInput;
+    public Animator _animatorRef;
+    Rigidbody2D _rigidB;
+    public float _speed;
+
+    void Awake()
+    {
+        _rigidB = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal"),
+              vertical = Input.GetAxisRaw("Vertical");
+
+        _movementInput = new Vector2(horizontal, vertical).normalized;
+
+        /*if( _movementInput.sqrMagnitude > _speed)
+        {
+            _animatorRef.SetFloat("dirX", orientation.x);
+            _animatorRef.SetFloat("dirY", orientation.y);
+        }*/
+
+        //Le personnage fais une roulade !
+        if (Input.GetButtonDown("Jump") && !_animatorRef.GetBool("touchRoll"))
+        {
+            _animatorRef.SetBool("touchRoll", true);
+        }
+
+        //Fais tournez le personnage sur lui meme !
+        if (Input.GetButtonDown("Turn") && !_animatorRef.GetBool("touchTurn"))
+        {
+            _animatorRef.SetBool("touchTurn", true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift)) { _speed *= 2f; }
+        if (Input.GetKeyUp(KeyCode.LeftShift)) { _speed /= 2f; }
+
+        _animatorRef.SetFloat("Horizontal", horizontal);
+        _animatorRef.SetFloat("Vertical", vertical);
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 velocity = _movementInput * _speed;
+        _rigidB.velocity = velocity;
+
+        _animatorRef.SetFloat("speedMove",Mathf.Abs(_rigidB.velocity.magnitude));
+    }
+}
